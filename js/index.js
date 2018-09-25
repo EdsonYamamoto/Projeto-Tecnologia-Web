4c3d6edfdd9e66a1de312370c95987bd5c28a683
Unknown <edsonkazyamamoto@gmail.com>
2018-09-25 00:36:37 -0300

    Adicionado moment

--- js/index.js
+++ js/index.js
@@ -1,36 +1,104 @@
 var count = 1;
+
+$(document).on('keypress', 'input.inputCPF', function(e) {
+  var key = (window.event)?event.keyCode:e.which;
+  if((key > 47 && key < 58)) {
+      return true;   
+    } 
+    else {
+     return (key == 8 || key == 0)?true:false;
+   }
+});
+
+$(function () {
+
+  function Excluir(){
+    var par = $(this).parent().parent();
+    if (confirm('Deseja excluir o registro?'))
+      par.remove();
+  };
+
+  function Editar(){
+    var par = $(this).parent().parent(); //tr
+    var tdNome = par.children("td:nth-child(1)");
+    var tdEmail = par.children("td:nth-child(2)");
+    var tdCPF = par.children("td:nth-child(3)");
+    var tdData = par.children("td:nth-child(4)");
+    var tdSexo = par.children("td:nth-child(5)");
+
+    tdNome.html("<input type ='text' value='"+tdNome.html()+"'/td>");
+    tdEmail.html("<input type='text' id='txtEmail' value='"+tdEmail.html()+"'/>");
+    tdCPF.html("<input type='text'id='txtDN' value='"+tdCPF.html()+"'/>");
+    tdData.html("<input type='text'id='txtDN' value='"+tdData.html()+"'/>");
+    tdSexo.html("<input type='text'id='txtDN' value='"+tdSexo.html()+"'/>");
+  }
+
+  function FunSal(){
+    var par = $(this).parent().parent(); //tr
+    var tdNome = par.children("td:nth-child(1)");
+    var tdEmail = par.children("td:nth-child(2)");
+    var tdCPF = par.children("td:nth-child(3)");
+    var tdData = par.children("td:nth-child(4)");
+    var tdSexo = par.children("td:nth-child(5)");
+
+    tdNome.html(tdNome.children("input[type=text]").val());
+    tdEmail.html(tdEmail.children("input[type=text]").val());
+    tdCPF.html(tdCPF.children("input[type=text]").val());
+    tdData.html(tdData.children("input[type=text]").val());
+    tdSexo.html(tdSexo.children("input[type=text]").val());
+  };
+
+  $("#tabCrud").on("click", ".ExBut", Excluir);
+  $("#tabCrud").on("click", ".AltBut", Editar);
+  $("#tabCrud").on("click", ".salBut", FunSal);
+});
+
 function met() {
 
-  var a = document.getElementById("IdTeste").innerHTML;
-  var inputText = $("#inputText").val();
+  var emailReg = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i);
+  var inputNome = $("#inputNome").val();
   var inputDate = $("#inputDate").val();
-  var inputSelect = $("#inputSelect").val();
-  console.log(inputText);
-  //devo pegar o valor atual do atributo e concatenar com outro texto
+  var inputSexo = $("#inputSexo").val();
+  var inputEmail = $("#inputEmail").val();
+  var inputCPF = $("#inputCPF").val();
 
-  a += (
-    '<p id=' + count +
-    '>Texto: <label id="idLabel' + count + '">' + inputText +
-    '</label> Data: <label id="idData' + count + '"> ' + inputDate +
-    '</label>  Select: <label id="idSelect' + count + '"> ' + inputSelect +
-    '</label> <button class="btnExcluir" onclick= metodoExcluir(' + this.count +
-    ')> exluir</button><button class="btnEditar" onclick= metodoEditar(' + this.count + ')> editar</button></p>');
+  var linha = $('<tr><td class="idNome" ' + count + '" id="idNome' + count + '">'+ inputNome +
+                '</td> <td  id="idEmail' + count + '"> ' + inputEmail + 
+                '</td> <td id="idCPF' + count + '">' + inputCPF + 
+                '</td> <td id="idData' + count + '">' + inputDate + 
+                '</td> <td id="idSexo' + count + '">' + inputSexo + 
+                '</td> <td><input type="button" class="ExBut" value="Excluir"/>' + 
+                '</td> <td><input type="button" class="AltBut" value="Alterar"/>' + 
+                '</td> <td><input type="button" class="salBut" value="Salvar"/></td></tr>'
+  )
+
   this.count++;
-  //em seguida armazeno novamente dentro da div de idTeste o conteudo armazenado.
-  document.getElementById("IdTeste").innerHTML = a;
-}
 
-function metodo() {
-  alert("estou aqui")
-}
+  if(inputNome == undefined || inputNome==""){
+    alert("O campo Nome é obrigatório")
+  }
+  else if(inputEmail == undefined || inputEmail=="")
+  {
+    alert("O campo E-mail é obrigatório!")
+  }
+  else if(!emailReg.test(inputEmail)){
+    alert("E-mail com formato invalido")
+  }
+  else if(inputCPF == undefined || inputCPF=="")
+  {
+    alert("O campo CPF é obrigatório!")
+  }
+  else if(inputDate == undefined || inputDate=="")
+  {
+    alert("Selecione uma data válida")
+  }
+  else{
+    $('#tabCrud').append(linha);
 
-function metodoExcluir(id) {
-  alert('exluir item: ' + id);
-  alert('exluir item: ' + $('#' + id).html());
-  'exluir item: ' + $('#' + id).html("");
-}
-
-function metodoEditar(id) {
-  alert('editar item: ' + id)
-  alert('editar item: ' + $("#" + id).html());
+    $("#inputNome").val("");
+    $("#inputEmail").val("");
+    $("#inputDate").val("");
+    $("#inputSexo").val("Homem");
+    $("#inputCPF").val("");
+  }
 }
